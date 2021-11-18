33e0b34a8a96e8617ea361f8a02cadea4758acea
Ada Birtocian <adabirtocian@yahoo.com>
2021-11-18 16:01:35 +0200

    http get request with callbacks

---
+++ Futures_continuations_lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿// A server that accepts pairs of numbers, transmitted as text and separated by whitespace, and sends back their sums
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Futures_continuations_lab4
+{
+    public static class Program
+    {
+        // adding 3 hosts, each returning a response in different format:
+        private static readonly List<string> HOSTS = new List<string> {
+            // - plain text
+            "www.google.com",
+        };
+
+        public static void Main(string[] args)
+        {
+            Callbacks.run(HOSTS);
+        }
+    }
+}
