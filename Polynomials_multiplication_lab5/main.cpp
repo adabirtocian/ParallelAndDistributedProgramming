f2ee96524d0f497a8d41f4478846f3ae2a62d807
Ada Birtocian <adabirtocian@yahoo.com>
2021-12-04 20:12:47 +0200

    generate random coefficients for polynomials

--- Polynomials_multiplication_lab5/main.cpp
+++ Polynomials_multiplication_lab5/main.cpp
@@ -2,6 +2,21 @@
 #include <vector>
 #include "RegularAlgorithm.hpp"
 
+std::vector<int> generatePolynomialCoefficients(int size)
+{
+	std::vector<int> polynomial(size);
+	srand(time(NULL));
+
+	for (int i = 0; i < size - 1; ++i)
+	{
+		polynomial[i] = rand() % 10;
+		std::cout << polynomial[i] << " * x^" << i << " + ";
+	}
+	polynomial[size - 1] = rand() % 10 + 1;
+	std::cout << polynomial[size - 1] << " * x^" << size - 1 << "\n";
+
+	return polynomial;
+}
 
 int main()
 {
@@ -10,27 +25,18 @@ int main()
 	std::cout << "Size polynomial 1: ";
 	std::cin >> size1;
 
-	std::cout << "\nSize polynomial 2: ";
+	std::cout << "Size polynomial 2: ";
 	std::cin >> size2;
 
-	std::vector<int> polynomial1(size1);
-	std::vector<int> polynomial2(size2);
-
 	std::cout << "Polynomial 1 coefficients: \n";
-	for (int i = 0; i < size1; ++i)
-	{
-		std::cin >> polynomial1[i];
-	}
+	std::vector<int> polynomial1 = generatePolynomialCoefficients(size1);
 
 	std::cout << "Polynomial 2 coefficients: \n";
-	for (int i = 0; i < size2; ++i)
-	{
-		std::cin >> polynomial2[i];
-	}
+	std::vector<int> polynomial2 = generatePolynomialCoefficients(size2);
 
 	RegularAlgorithm regularAlgo = RegularAlgorithm(polynomial1, polynomial2, size1, size2);
 	regularAlgo.multiply();
-	std::cout << regularAlgo.getPolynomialResult();
+	std::cout << "Result: \n" << regularAlgo.getPolynomialResult();
 
 	return 0;
 }
