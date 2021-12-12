7c8d540f515488f0dbf7272ffc2ef67b9e94fa3a
Ada Birtocian <adabirtocian@yahoo.com>
2021-12-12 15:07:43 +0200

    fix atomic error; add more examples

--- Hamiltonean_cycle_lab6/DirectedGraph.cpp
+++ Hamiltonean_cycle_lab6/DirectedGraph.cpp
@@ -1,17 +1,18 @@
 #include "DirectedGraph.hpp"
 #include <iostream>
 #include <algorithm>
+#include <future>
+#include <thread>
 
-
-DirectedGraph::DirectedGraph(std::vector<Node*> nodes): hasHamiltoneanCycle{false}, nodes{nodes}
+DirectedGraph::DirectedGraph(std::vector<Node*> nodes): nodes{nodes}, hasHamiltoneanCycle(0)
 {
 }
 
-void DirectedGraph::findHamiltoneanCycleRecursive(Node& node, std::vector<Node*> visitedNodes)
+void DirectedGraph::findHamiltoneanCycleRecursive(Node& node, std::vector<Node*> visitedNodes, int threads)
 {
 	if (std::find(visitedNodes.begin(), visitedNodes.end(), &node) != visitedNodes.end())
 	{
-		this->hasHamiltoneanCycle = true;
+		this->hasHamiltoneanCycle = this->hasHamiltoneanCycle.fetch_or(1);
 		this->printCycle(node, visitedNodes);
 		return;
 	}
@@ -19,15 +20,39 @@ void DirectedGraph::findHamiltoneanCycleRecursive(Node& node, std::vector<Node*>
 	node.setVisited(true);
 	visitedNodes.push_back(&node);
 	
-	for (int i = 0; i < node.getGoToNodes().size(); ++i)
+	if (threads > 1)
 	{
-		if (this->hasHamiltoneanCycle)
+		for (int i = 0; i < node.getGoToNodes().size(); ++i)
 		{
-			return;
+			if (this->hasHamiltoneanCycle)
+			{
+				return;
 
+			}
+			Node* goToNode = node.getGoToNodes()[i];
+			this->findHamiltoneanCycleRecursive(*goToNode, visitedNodes, threads);
 		}
-		Node* goToNode = node.getGoToNodes()[i];
-		this->findHamiltoneanCycleRecursive(*goToNode, visitedNodes);
+	}
+	else
+	{
+		// One thread available. Continue the search sequentially.
+
+		for (int i = 0; i < node.getGoToNodes().size() - 1; i+=2)
+		{
+			if (this->hasHamiltoneanCycle)
+			{
+				return;
+
+			}
+			Node* goToNode = node.getGoToNodes()[i];
+			std::future<void> other = std::async(std::launch::async, [this, goToNode, visitedNodes, threads]() { this->findHamiltoneanCycleRecursive(*goToNode, visitedNodes, threads / 2);  });
+			
+			goToNode = node.getGoToNodes()[i + 1];
+			this->findHamiltoneanCycleRecursive(*goToNode, visitedNodes, threads - threads / 2);
+			
+			other.wait();
+		}
+		this->findHamiltoneanCycleRecursive(*node.getGoToNodes()[node.getGoToNodes().size()-1], visitedNodes, threads - threads / 2);
 	}
 }
 
@@ -38,7 +63,7 @@ void DirectedGraph::findHamiltoneanCycle(int threads)
 		if (!node->getVisited())
 		{
 			std::vector<Node*> visitedNodes;
-			this->findHamiltoneanCycleRecursive(*node, visitedNodes);
+			this->findHamiltoneanCycleRecursive(*node, visitedNodes, threads);
 		}
 	}
 }
@@ -46,11 +71,12 @@ void DirectedGraph::findHamiltoneanCycle(int threads)
 void DirectedGraph::printCycle(Node& node, std::vector<Node*> visitedNodes)
 {
 	auto it = std::find(visitedNodes.begin(), visitedNodes.end(), &node);
+	std::cout << "Hamiltonean cycle: ";
 
 	for (auto i = it; i != visitedNodes.end(); ++i)
 	{
 		std::cout << **i;
 	}
 	
-	std::cout << node;
+	std::cout << node << "\n";
 }
--- Hamiltonean_cycle_lab6/DirectedGraph.hpp
+++ Hamiltonean_cycle_lab6/DirectedGraph.hpp
@@ -1,15 +1,17 @@
 #pragma once
 #include <vector>
 #include <stack>
+#include <atomic>
 #include "Node.hpp"
 
 class DirectedGraph
 {
 private:
 	std::vector<Node*> nodes;
-	bool hasHamiltoneanCycle;
+	std::atomic<int> hasHamiltoneanCycle;
+	//int hasHamiltoneanCycle;
 
-	void findHamiltoneanCycleRecursive(Node& node, std::vector<Node*> visitedNodes);
+	void findHamiltoneanCycleRecursive(Node& node, std::vector<Node*> visitedNodes, int threads);
 
 public:
 	DirectedGraph(std::vector<Node*> nodes);
