8e8c8930cf61edda37167de08da65d5ec73d566f
Ada Birtocian <adabirtocian@yahoo.com>
2021-12-21 21:04:56 +0200

    send receive workflow for regular algorithm

--- MPI_polynomials_multiplication_lab7/main.cpp
+++ MPI_polynomials_multiplication_lab7/main.cpp
@@ -4,9 +4,6 @@
 #include <vector>
 #include <chrono>
 #include <string>
-#include "Multiplication.hpp"
-#include "RegularMultiplication.hpp"
-#include "KaratsubaMultiplication.hpp"
 
 std::vector<int> generatePolynomialCoefficients(int size)
 {
@@ -20,6 +17,56 @@ std::vector<int> generatePolynomialCoefficients(int size)
     polynomial[size - 1] = rand() % 4 + 1;
 
     return polynomial;
+}
+
+void regularWorker(int processId)
+{
+    MPI_Status status;
+    int start, end, size1, size2;
+    std::vector<int> p1;
+    std::vector<int> p2;
+
+
+    MPI_Recv(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    
+    MPI_Recv(&size1, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    p1.resize(size1);
+    MPI_Recv(p1.data(), size1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+    
+    MPI_Recv(&size2, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    p2.resize(size2);
+    MPI_Recv(p2.data(), size2, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+
+    std::cout << "Worker "<<processId << " " << start;// << " " << end << " " << size1 << " " << size2 << "\n";
+}
+
+void regularMaster(int processes, std::vector<int> polynomial1, std::vector<int> polynomial2)
+{
+    std::cout << "Master starts\n";
+    int workerLength = (polynomial1.size() + polynomial2.size() - 1) / (processes - 1);
+    int size1 = polynomial1.size();
+    int size2 = polynomial2.size();
+
+    // split work between workers
+    for (int i = 1; i < processes; ++i)
+    {
+        int start = i * workerLength;
+        int end = start + workerLength - 1;
+
+        std::cout << "Master sends: " << start;// << " " << end << " " << size1 << " " << size2 << "\n";
+        // send the indices
+        MPI_Send(&start, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+        MPI_Ssend(&end, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+
+        // send the data 
+        MPI_Ssend(&size1, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+        MPI_Ssend(polynomial1.data(), size1, MPI_INT, i, 2, MPI_COMM_WORLD);
+        MPI_Ssend(&size2, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+        MPI_Ssend(polynomial2.data(), size2, MPI_INT, i, 2, MPI_COMM_WORLD);
+    }
+
+    // compute first coefficients of the result
 }
 
 int main(int argc, char** argv) 
@@ -47,7 +94,6 @@ int main(int argc, char** argv)
     }
     
     std::string method = argv[3];
-    Multiplication* multiplication = new RegularMultiplication();
 
     if (method != "regular" && method != "karatsuba")
     {
@@ -62,21 +108,31 @@ int main(int argc, char** argv)
         std::vector<int> polynomial1 = generatePolynomialCoefficients(size1);
         std::vector<int> polynomial2 = generatePolynomialCoefficients(size2);
         std::cout << "Polynomials generated with sizes "<<size1 << " and " << size2 << "\n";
-
+        for (int i : polynomial1)
+        {
+            std::cout << i << " ";
+        }
+        std::cout << "\n";
+        for (int i : polynomial2)
+        {
+            std::cout << i << " ";
+        }
+        // set the method to be used in multiplication
         if (method == "regular")
         {
-            multiplication = new RegularMultiplication(polynomial1, polynomial2);
+            std::cout << "start\n";
+            regularMaster(processes, polynomial1, polynomial2);
         }
         else if (method == "karatsuba")
         {
-            multiplication = new KaratsubaMultiplication(polynomial1, polynomial2);
+            
         }
-        multiplication->master();
+
     }
     else
     {
         // worker process
-
+        regularWorker(currentProcessRank);
     }
 
     // Finalize the MPI environment.
