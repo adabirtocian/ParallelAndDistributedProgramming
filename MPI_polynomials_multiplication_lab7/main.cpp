d5c7fe645df00e8f30b648a204766d4a823301d6
Ada Birtocian <adabirtocian@yahoo.com>
2021-12-21 22:16:50 +0200

    regular polynomial multiplication

--- MPI_polynomials_multiplication_lab7/main.cpp
+++ MPI_polynomials_multiplication_lab7/main.cpp
@@ -5,56 +5,91 @@
 #include <chrono>
 #include <string>
 
-std::vector<int> generatePolynomialCoefficients(int size)
+std::vector<int> generatePolynomialCoefficients(int size, int additional=0)
 {
     std::vector<int> polynomial(size);
     srand(time(NULL));
 
-    for (int i = 0; i < size - 1; ++i)
+    for (int i = 0; i < size; ++i)
     {
-        polynomial[i] = rand() % 4 + 1;
+        polynomial[i] = rand() % (i+1) + 1 + additional;
     }
-    polynomial[size - 1] = rand() % 4 + 1;
 
     return polynomial;
+}
+
+
+std::vector<int> multiplySection(int size1, int size2,int start, int end, std::vector<int> polynomial1, std::vector<int> polynomial2)
+{
+    std::vector<int> partialResult = std::vector<int>(size1 + size2 - 1, 0);
+
+    for (int i = start; i <= end; ++i)
+    {
+        for (int j = 0; j < size2; ++j)
+        {
+            partialResult[i + j] += polynomial1[i] * polynomial2[j];
+            //std::cout << i + j << "=" << partialResult[i + j] << "\n";
+        }
+    }
+
+    return partialResult;
+}
+
+std::vector<int> aggregateResults(std::vector<int> master, std::vector<int> worker)
+{
+    std::vector<int> result = std::vector<int>(master.size(), 0);
+    for (int i = 0; i < master.size(); ++i)
+    {
+        result[i] += master[i] + worker[i];
+    }
+
+    return result;
 }
 
 void regularWorker(int processId)
 {
     MPI_Status status;
     int start, end, size1, size2;
-    std::vector<int> p1;
-    std::vector<int> p2;
-
+    std::vector<int> polynomial1;
+    std::vector<int> polynomial2;
 
     MPI_Recv(&start, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
     MPI_Recv(&end, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
     
     MPI_Recv(&size1, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-    p1.resize(size1);
-    MPI_Recv(p1.data(), size1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+    polynomial1.resize(size1);
+    MPI_Recv(polynomial1.data(), size1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
     
     MPI_Recv(&size2, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-    p2.resize(size2);
-    MPI_Recv(p2.data(), size2, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+    polynomial2.resize(size2);
+    MPI_Recv(polynomial2.data(), size2, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
 
-    std::cout << "Worker "<<processId << " " << start;// << " " << end << " " << size1 << " " << size2 << "\n";
+    std::cout << "Worker "<<processId << " " << start << " " << end << " " << size1 << " " << size2 << "\n";
+
+    // do the computations
+    std::vector<int> partialResult = multiplySection(size1, size2, start, end, polynomial1, polynomial2);
+
+    // send the result back to master
+    MPI_Ssend(partialResult.data(), size1+size2, MPI_INT, 0, 2, MPI_COMM_WORLD);
 }
 
 void regularMaster(int processes, std::vector<int> polynomial1, std::vector<int> polynomial2)
 {
-    std::cout << "Master starts\n";
-    int workerLength = (polynomial1.size() + polynomial2.size() - 1) / (processes - 1);
     int size1 = polynomial1.size();
     int size2 = polynomial2.size();
+    int workerLength = size1 / processes;
 
     // split work between workers
     for (int i = 1; i < processes; ++i)
     {
         int start = i * workerLength;
         int end = start + workerLength - 1;
+        if (i == processes - 1)
+        {
+            end = size1 - 1;
+        }
 
-        std::cout << "Master sends: " << start;// << " " << end << " " << size1 << " " << size2 << "\n";
+        std::cout << "Master sends: " << start << " " << end << " " << size1 << " " << size2 << "\n";
         // send the indices
         MPI_Send(&start, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
         MPI_Ssend(&end, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
@@ -66,7 +101,20 @@ void regularMaster(int processes, std::vector<int> polynomial1, std::vector<int>
         MPI_Ssend(polynomial2.data(), size2, MPI_INT, i, 2, MPI_COMM_WORLD);
     }
 
-    // compute first coefficients of the result
+    // do master part of the computation
+    std::vector<int> finalResult = multiplySection(size1, size2, 0, workerLength-1, polynomial1, polynomial2);
+    MPI_Status status;
+    std::vector<int> workerResult = std::vector<int>(size1 + size2, 0);
+
+    for (int i = 1; i < processes; ++i)
+    {
+        MPI_Recv(workerResult.data(), size1 + size2, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+        finalResult = aggregateResults(finalResult, workerResult);
+    }
+    for (int i = 0; i < finalResult.size(); ++i)
+    {
+        std::cout << finalResult[i] << " ";
+    }
 }
 
 int main(int argc, char** argv) 
@@ -86,7 +134,6 @@ int main(int argc, char** argv)
         fprintf(stderr, "Help: <.exe> <size1> <size2> <method(regular/karatsuba)>\n");
         return 1;
     }
-
     if(1 != sscanf_s(argv[1], "%u", &size1) || 1 != sscanf_s(argv[2], "%u", &size2)) 
     {
         fprintf(stderr, "Polynomials sizes must be given as args\n");
@@ -94,7 +141,6 @@ int main(int argc, char** argv)
     }
     
     std::string method = argv[3];
-
     if (method != "regular" && method != "karatsuba")
     {
         fprintf(stderr, "Invalid method! Choose from: regular / karatsuba\n");
@@ -106,7 +152,7 @@ int main(int argc, char** argv)
         // master process
         // generate 2 polynomials
         std::vector<int> polynomial1 = generatePolynomialCoefficients(size1);
-        std::vector<int> polynomial2 = generatePolynomialCoefficients(size2);
+        std::vector<int> polynomial2 = generatePolynomialCoefficients(size2, 5);
         std::cout << "Polynomials generated with sizes "<<size1 << " and " << size2 << "\n";
         for (int i : polynomial1)
         {
@@ -120,19 +166,24 @@ int main(int argc, char** argv)
         // set the method to be used in multiplication
         if (method == "regular")
         {
-            std::cout << "start\n";
             regularMaster(processes, polynomial1, polynomial2);
         }
         else if (method == "karatsuba")
         {
             
         }
-
     }
     else
     {
         // worker process
-        regularWorker(currentProcessRank);
+        if (method == "regular")
+        {
+            regularWorker(currentProcessRank);
+        }
+        else if (method == "karatsuba")
+        {
+
+        }
     }
 
     // Finalize the MPI environment.
